1e54d2c93640afd77866b89b67cbe36c73b730d0
Krasnova.EA <krasnovaea95@yandex.ru>
2018-04-09 20:20:28 +0300

    I'm developer

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,6 +1,6 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("Hello, my lovely Java!");
-        System.out.println("I will become a developer");
+        System.out.println("I will become a Java developer");
     }
 }
